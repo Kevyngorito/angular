019cbfcbb3ce15dc36114887adfddfdadd40218a
Kevyn Gorito <kevyndccap@gmail.com>
2022-04-12 15:40:54 -0300

    Inserindo navbard e app-nav-bar

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -4,16 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CourseListComponent } from './courses/course-list.component';
-import { Course } from './courses/course';
 import { StarComponent } from './courses/star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent
     
   ],
   imports: [
